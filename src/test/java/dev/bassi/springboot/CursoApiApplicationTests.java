0b8260e55d2a20aa7db80f55a58f331715ed7918
Matheus Bassi <bassi.matheus@scalasystems.com.br>
2023-02-05 02:42:53 -0300

    first commit

---
+++ src/test/java/dev/bassi/springboot/CursoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bassi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
